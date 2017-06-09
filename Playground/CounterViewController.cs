8fab2b6e896fbcbe3f71bf4fe81a29848f574917
gomi_ningen <gomi2ngen@gmail.com>
2017-06-09 23:16:07 +0900

    Remove storyboaard

---
+++ Playground/CounterViewController.cs
@@ -0,0 +1,14 @@
+﻿using UIKit;
+
+namespace Playground
+{
+    public class CounterViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            EdgesForExtendedLayout = UIRectEdge.None;
+            View.BackgroundColor = UIColor.Blue;
+        }
+    }
+}
